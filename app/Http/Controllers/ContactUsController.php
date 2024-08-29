13db3b2ee6339c69f40b4791034680f4c34398f9
abdullahadaileh <abdullahadaileh957@gmail.com>
2024-08-29 23:36:31 +0300

    first

---
+++ app/Http/Controllers/ContactUsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contact_us;
+use Illuminate\Http\Request;
+
+class ContactUsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Contact_us $contact_us)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Contact_us $contact_us)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Contact_us $contact_us)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Contact_us $contact_us)
+    {
+        //
+    }
+}
