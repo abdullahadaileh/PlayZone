9bf4abd2ca596eb04f2a75e43ee1e669cce73f82
zaina-alhelo <zainaalhelo00@gmail.com>
2024-08-30 18:11:20 +0300

    route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,13 @@
 <?php
 
+use App\Http\Controllers\BookingController;
+use App\Http\Controllers\ContactUsController;
+use App\Http\Controllers\FieldController;
+use App\Http\Controllers\FieldHistoryController;
+use App\Http\Controllers\FieldImagesController;
+use App\Http\Controllers\FieldTypeController;
+use App\Http\Controllers\SportTypeController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -14,10 +22,56 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// ** controllers ** //
+
+// Routes for UserController
+Route::resource('users', UserController::class);
+
+// Routes for BookingController
+Route::resource('bookings', BookingController::class);
+
+// Routes for ContactUsController
+Route::resource('contact-us', ContactUsController::class);
+
+// Routes for FieldController
+Route::resource('fields', FieldController::class);
+
+// Routes for FieldHistoryController
+Route::resource('field-histories', FieldHistoryController::class);
+
+// Routes for FieldImagesController
+Route::resource('field-images', FieldImagesController::class);
+
+// Routes for FieldTypeController
+Route::resource('field-types', FieldTypeController::class);
+
+// Routes for SportTypeController
+Route::resource('sport-types', SportTypeController::class);
+
+
+
+/////landing_page
+
+
+Route::get('/', function () {
+    return view('landing_page.landing');
+})->name('Home');
+
+Route::get('/contact', function () {
+    return view('landing_page.pages.contact');
+})->name('contact');
+
+Route::get('/about', function () {
+    return view('landing_page.pages.about');
+})->name('about');
+Route::get('/services', function () {
+    return view('landing_page.pages.services');
+})->name('services');
+Route::get('/blogs', function () {
+    return view('landing_page.pages.blogs');
+})->name('blogs');
