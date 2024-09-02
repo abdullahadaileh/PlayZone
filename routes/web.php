157a50efcb5a96a719ae7e3d8f596b0ae22ac1a3
abdullahadaileh <abdullahadaileh957@gmail.com>
2024-09-02 14:21:47 +0300

    update

--- routes/web.php
+++ routes/web.php
@@ -38,7 +38,7 @@ Route::resource('users', UserController::class);
 Route::resource('bookings', BookingController::class);
 
 // Routes for ContactUsController
-Route::resource('contact-us', ContactUsController::class);
+Route::post('/contact/store', [ContactUsController::class, 'store'])->name('contact.store'); // تعديل
 
 // Routes for FieldController
 Route::resource('fields', FieldController::class);
@@ -84,10 +84,9 @@ Route::get('/dash', function () {
 });
 
 
-Route::get('/dash', [DashboardController::class, 'index'])->middleware(['auth','admin']);
-Route::get('/dash', [DashboardController::class, 'index'])->name('dashboard.main');
+Route::get('/dash', [DashboardController::class, 'index'])->middleware(['auth','admin'])->name('dashboard.main');
 // Create user
-Route::get('/dashboard/create-user', [UserController::class, 'create'])->name('Dashboard.create-user');
+Route::get('/dash/create-user', [UserController::class, 'create'])->name('dash.create-user');
 Route::middleware(['auth'])->group(function () {
     // Delete user
 Route::delete('users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
@@ -95,4 +94,7 @@ Route::delete('users/{id}', [UserController::class, 'destroy'])->name('users.des
 // Edit user
 Route::get('/users/{id}/edit-role', [UserController::class, 'editRole'])->name('users.editRole');
 Route::put('/users/{id}/update-role', [UserController::class, 'updateRole'])->name('users.updateRole');
+
+// contact us
+Route::get('/dash/contact-us', [ContactUsController::class, 'index'])->name('dashboard.contact_us');
 
